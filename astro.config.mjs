395909f53f89d02091876c62c0d2234e330d205b
Sooraj Sivadasan <cherrycheeks404@gmail.com>
2025-03-12 15:38:16 +0530

    changed the deployment url

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321/',
+  site: 'https://soorajts.vercel.app/',
   integrations: [expressiveCode(expressiveCodeOptions), tailwind({
     applyBaseStyles: false
   }),sitemap(), icon(),mdx()],
